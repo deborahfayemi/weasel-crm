c1703ba353db8cec5e4849a0f548b03a84e08e40
Deborah <busayomifayemi@gmail.com>
2023-11-13 14:29:16 -0600

    Update outputs.tf
    adding definitions to the outputs

---
+++ template/outputs.tf
@@ -0,0 +1,11 @@
+output "ec2_instance_ids" {
+  value = aws_instance.weasel_ec2.*.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.weasel_db.address
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.weasel_s3.bucket
+}
