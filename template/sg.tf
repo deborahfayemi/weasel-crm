784514b3974263b4599df7913fc88960bda97abe
Deborah <busayomifayemi@gmail.com>
2023-11-13 21:41:08 -0600

    Update sg.tf
    added ingress rule for ec2 ssh

--- template/sg.tf
+++ template/sg.tf
@@ -5,6 +5,13 @@ resource "aws_security_group" "weasel_sg_web" {
 
   # Ingress rules define the incoming traffic to the EC2 instances.
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_cidr_blocks_ssh
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
