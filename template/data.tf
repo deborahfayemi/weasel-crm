23e720afbc47400865aba420b75c0c21cb8ed5e3
Deborah <busayomifayemi@gmail.com>
2023-11-15 00:57:02 -0600

    Streamline Data Sources for VPC, Subnets, and Internet Gateway
    - Updated `aws_vpc` data sources:
      - `selected`: Configured to be created when `var.use_default_vpc` is false. It fetches details of the VPC tagged with `var.vpc_tag_name`.
      - `default`: Conditionally created when `var.use_default_vpc` is true, retrieving the default VPC.
    
    - Retained `aws_availability_zones` data source `available`:
      - Continues to provide information on availability zones in the "available" state.
    
    - Updated `aws_subnet_ids` data sources:
      - `selected_subnets`: Fetches subnet IDs for the non-default VPC when `var.use_default_vpc` is false.
      - `default_subnets`: Retrieves subnet IDs for the default VPC when `var.use_default_vpc` is true.
    
    - Kept the `aws_internet_gateway` data source `existing_igw`:
      - Filters and fetches the Internet Gateway attached to the VPC specified in `var.vpc_id`.
    
    - Removed other data sources that were previously part of the configuration.

--- template/data.tf
+++ template/data.tf
@@ -1,21 +1,10 @@
-data "aws_vpc" "selected" {
-  count = var.use_default_vpc ? 0 : 1
-  tags = {
-    Name = var.vpc_tag_name
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_internet_gateway" "existing_igw" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [var.vpc_id]
   }
-}
-
-data "aws_vpc" "default" {
-  count   = var.use_default_vpc ? 1 : 0
-  default = true
-}
-
-data "aws_subnet_ids" "selected_subnets" {
-  count  = var.use_default_vpc ? 0 : 1
-  vpc_id = data.aws_vpc.selected[0].id
-}
-
-data "aws_subnet_ids" "default_subnets" {
-  count  = var.use_default_vpc ? 1 : 0
-  vpc_id = data.aws_vpc.default[0].id
 }
