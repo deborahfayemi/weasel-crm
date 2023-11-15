9a7082cd4a1ad3616689c5152ea359b973fe7611
Deborah <busayomifayemi@gmail.com>
2023-11-15 00:45:41 -0600

    Refine RDS MySQL Instance Configuration
    - Modified `aws_db_instance` resource configuration for `weasel_db`:
      - Updated attributes including `allocated_storage`, `max_allocated_storage`, `storage_type`, `engine`, `engine_version`, and `instance_class` with values sourced from corresponding variables.
      - Set the `vpc_security_group_ids` to include the ID of the `aws_security_group.weasel_sg_db`.
      - Defined the `db_subnet_group_name` using `aws_db_subnet_group.weasel_db_subnet_group.name`.
    
    - Changed the database identifier properties:
      - Updated the `db_name` to `"${var.common_app_name}_db"` from the previous `"${var.common_app_name}-db"`.
      - Added a new `identifier` attribute as `"${var.common_app_name}-db"` for the RDS instance.
    
    - Revised the database snapshot and availability configurations:
      - Retained `parameter_group_name` and `skip_final_snapshot` attributes with values from variables.
      - Added `final_snapshot_identifier` to generate a unique snapshot name using the current timestamp.
      - Set the `multi_az` configuration to enhance database availability, controlled by `var.rds_multi_az`.
    
    - Tags and other configurations remained unchanged.
    
    These updates enhance the RDS MySQL instance's configurability, particularly in terms of security, snapshot management, and high availability setup.

--- template/rds.tf
+++ template/rds.tf
@@ -1,19 +1,24 @@
 # Resource block for the RDS MySQL database instance
 resource "aws_db_instance" "weasel_db" {
-  allocated_storage     = var.db_allocated_storage
-  max_allocated_storage = var.db_max_allocated_storage
-  storage_type          = var.db_storage_type
-  engine                = var.db_engine
-  engine_version        = var.db_engine_version
-  instance_class        = var.rds_instance_class
+  allocated_storage      = var.db_allocated_storage
+  max_allocated_storage  = var.db_max_allocated_storage
+  storage_type           = var.db_storage_type
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  instance_class         = var.rds_instance_class
+  vpc_security_group_ids = [aws_security_group.weasel_sg_db.id]
+  db_subnet_group_name   = aws_db_subnet_group.weasel_db_subnet_group.name
 
   # Database identifier.
-  db_name  = "${var.common_app_name}-db"
-  username = var.db_username
-  password = var.db_password
+  db_name    = "${var.common_app_name}_db"
+  identifier = "${var.common_app_name}-db"
+  username   = var.db_username
+  password   = var.db_password
 
-  parameter_group_name = var.parameter_group_name
-  skip_final_snapshot  = var.skip_final_snapshot
+  parameter_group_name      = var.parameter_group_name
+  skip_final_snapshot       = var.skip_final_snapshot
+  final_snapshot_identifier = "${var.common_app_name}-final-snapshot-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+  multi_az                  = var.rds_multi_az
 
   # Tags.
   tags = {
