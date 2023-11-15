5a6b4f4c0d89b0667f24a34b0c5527f9e2743311
Deborah <busayomifayemi@gmail.com>
2023-11-15 00:48:56 -0600

    Add Outputs for S3 Bucket and ALB Resources
    - Added output definitions in Terraform configuration:
      - `s3_bucket_name`:
        - Outputs the name of the S3 bucket created by `aws_s3_bucket.weasel_s3`. This facilitates easy access to the bucket name for other resources or modules.
      - `alb_arn`:
        - Outputs the Amazon Resource Name (ARN) of the Application Load Balancer created by `aws_lb.weasel_alb`. The ARN can be used in other AWS services or Terraform modules requiring this identifier.
      - `alb_dns_name`:
        - Outputs the DNS name of the Application Load Balancer.

--- template/outputs.tf
+++ template/outputs.tf
@@ -9,3 +9,11 @@ output "rds_endpoint" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.weasel_s3.bucket
 }
+
+output "alb_arn" {
+  value = aws_lb.weasel_alb.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.weasel_alb.dns_name
+}
